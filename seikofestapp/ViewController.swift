412f14b37dcc3d1dd5b21b19313c415f1caab737
tanahaya <tanahaya00623@gmail.com>
2016-12-25 09:59:21 +0900

    Initial Commit

---
+++ seikofestapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seikofestapp
+//
+//  Created by 田中千洋 on 2016/12/25.
+//  Copyright © 2016年 田中 颯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
